36c437cdcbcc4f13e59403749dbc69de29202991
Jake Mills <jake.mills.05@gmail.com>
2020-07-08 15:59:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,7 +13,8 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => [
-        
+        "Turquoise",
+        "Purple"
       ],
       :players => [
         
